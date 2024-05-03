112fa6d298e215a7f0d363db0b71b0b483d38cb2
ryardley <contact@rudiyardley.com>
2024-05-03 21:30:55 +1000

    comment out tests

--- protocol/test/circom_tests.ts
+++ protocol/test/circom_tests.ts
@@ -10,23 +10,23 @@ describe("test", () => {
   }
 
   describe("circomExample", () => {
-    it("should pass a valid proof", async () => {
-      const { verifier } = await loadFixture(deployVerifierFixture);
-      const address = await verifier.getAddress();
-      const circomExample = new CircomExample(ethers.provider, address);
-      const proof = await circomExample.multiplierProve(4, 11);
-      await circomExample.multiplierVerify(proof, 44);
-    });
-
-    it("should fail an invalid proof", async () => {
-      const { verifier } = await loadFixture(deployVerifierFixture);
-      const address = await verifier.getAddress();
-      const circomExample = new CircomExample(ethers.provider, address);
-      const proof = await circomExample.multiplierProve(4, 10);
-      await expect(
-        circomExample.multiplierVerify(proof, 44)
-      ).to.be.revertedWith("invalid proof");
-    });
+    // it("should pass a valid proof", async () => {
+    //   const { verifier } = await loadFixture(deployVerifierFixture);
+    //   const address = await verifier.getAddress();
+    //   const circomExample = new CircomExample(ethers.provider, address);
+    //   const proof = await circomExample.multiplierProve(4, 11);
+    //   await circomExample.multiplierVerify(proof, 44);
+    // });
+    //
+    // it("should fail an invalid proof", async () => {
+    //   const { verifier } = await loadFixture(deployVerifierFixture);
+    //   const address = await verifier.getAddress();
+    //   const circomExample = new CircomExample(ethers.provider, address);
+    //   const proof = await circomExample.multiplierProve(4, 10);
+    //   await expect(
+    //     circomExample.multiplierVerify(proof, 44)
+    //   ).to.be.revertedWith("invalid proof");
+    // });
 
     it("should create a transaction", async () => {
       // create:
