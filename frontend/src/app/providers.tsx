d9970f86a5bb5d4a15c6a070efd5df373af53942
ryardley <contact@rudiyardley.com>
2024-05-03 21:28:21 +1000

    Revert "Setup frontend"
    This reverts commit 2c765e5f2589eb32f3e59abb1aa36478b741f7c2.

--- frontend/src/app/providers.tsx
+++
@@ -1,8 +0,0 @@
-// app/providers.tsx
-"use client";
-
-import { ChakraProvider } from "@chakra-ui/react";
-
-export function Providers({ children }: { children: React.ReactNode }) {
-    return <ChakraProvider>{children}</ChakraProvider>;
-}
