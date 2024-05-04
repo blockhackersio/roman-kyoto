d8878640847a514bc738c25d5bfce2b699fb7f80
Ben Hooper <ben@labrys.io>
2024-05-04 20:36:06 +1000

    feat: migrated to hardhat deploy

--- protocol/test/privacy-token.test.ts
+++ protocol/test/privacy-token.test.ts
@@ -5,35 +5,25 @@ import {
   SpendVerifier,
   RK,
   RK__factory,
-  ERC20,
-  USDC,
-  WBTC,
-  USDC__factory,
-  WBTC__factory,
 } from "../typechain-types";
+import MerkleTree from "fixed-merkle-tree";
 
 import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
 import {
   withdraw,
   Note,
   getBabyJubJub,
+  getRandomBits,
+  notecommitment,
   deposit,
+  getInitialPoints,
   getAsset,
+  toStr,
   transfer,
-  buildMerkleTree,
-  getKeys,
-  decryptNote,
-  nullifierHash,
+  CircomStuff,
 } from "../src";
 import { poseidonHash, poseidonHash2 } from "../src/poseidon";
-import {
-  LogDescription,
-  Contract,
-  ContractTransactionReceipt,
-  Interface,
-  Wallet,
-  parseUnits,
-} from "ethers";
+import { AbiCoder, keccak256, randomBytes } from "ethers";
 import { generateGroth16Proof, toFixedHex } from "../src/zklib";
 import { expect } from "chai";
 import { ensureBytes } from "@noble/curves/abstract/utils";
@@ -47,10 +37,10 @@ describe("CCIP functionality testing", async () => {
   // for our testing purposes, the CCIP router can just be a signer
   let CCIPRouter: SignerWithAddress;
 
+  let MultiplierVerifierSource: MultiplierVerifier;
+  let OutputVerifierSource: OutputVerifier;
+  let SpendVerifierSource: SpendVerifier;
   let RK: RK;
-
-  let testUSDC: USDC;
-  let testWBTC: WBTC;
 
   let rkAddress: string;
 
@@ -61,6 +51,10 @@ describe("CCIP functionality testing", async () => {
 
     [Deployer, CCIPRouter] = await ethers.getSigners();
 
+    const MultiplierVerifierSource = await hre.deployments.get(
+      "MultiplierVerifierSource"
+    );
+
     const OutputVerifierSource = await hre.deployments.get(
       "OutputVerifierSource"
     );
@@ -68,20 +62,6 @@ describe("CCIP functionality testing", async () => {
     const SpendVerifierSource = await hre.deployments.get(
       "SpendVerifierSource"
     );
-
-    const usdcAddress = (await hre.deployments.get("USDC")).address;
-    testUSDC = new ethers.Contract(
-      usdcAddress,
-      USDC__factory.abi,
-      Deployer
-    ) as unknown as USDC;
-
-    const wbtcAddress = (await hre.deployments.get("WBTC")).address;
-    testWBTC = new ethers.Contract(
-      wbtcAddress,
-      WBTC__factory.abi,
-      Deployer
-    ) as unknown as WBTC;
 
     // deploy our EdOnBN254 library
     await hre.deployments.deploy("EdOnBN254", {
@@ -118,231 +98,225 @@ describe("CCIP functionality testing", async () => {
       RK__factory.abi,
       Deployer
     ) as unknown as RK;
-
-    // tell our protocol these erc20s are supported
-    await RK.addSupportedAsset(await getAsset("USDC"), usdcAddress, 6);
-    await RK.addSupportedAsset(await getAsset("WBTC"), wbtcAddress, 18);
   });
 
-  type WalletStore = {
-    notes: { note: Note; nullifier: string; index: bigint }[];
-    nullifiers: string[];
-    privateKey: string;
-    getNotesUpTo: (amount: bigint, asset: string) => Promise<Note[]>;
-    getBalance: (asset: string) => Promise<bigint>;
-    logBalances: () => Promise<void>;
-  };
+  it("bind siggies", async () => {
+    const message = "Hello world";
+    const msgBytes = ensureBytes("message", Buffer.from(message, "utf8"));
 
-  const getUnspentNotes = (store: WalletStore) => {
-    return store.notes.filter((note) => {
-      return !store.nullifiers.includes(note.nullifier);
+    const { modN, reddsaSign } = getInitialPoints(babyJub);
+
+    const a = modN(BigInt("0x" + Buffer.from(randomBytes(32)).toString("hex")));
+    const A = babyJub.ExtendedPoint.BASE.multiply(a);
+
+    const msgByteStr = toFixedHex(msgBytes);
+    const { s, R } = reddsaSign(a, A, msgByteStr);
+
+    /////////////////////////////
+    // sig is now R and s
+    //////////////////////////////
+    // Following should happen in solidity
+    //
+    const abi = new AbiCoder();
+    const cData = abi.encode(
+      ["uint256", "uint256", "uint256", "uint256", "bytes"],
+      [R.x, R.y, A.x, A.y, msgByteStr]
+    );
+    const hashed = keccak256(cData);
+    const c = modN(BigInt(hashed));
+    const B = babyJub.ExtendedPoint.BASE;
+
+    expect(
+      B.negate()
+        .multiply(s)
+        .add(R)
+        .add(A.multiply(c))
+        .equals(babyJub.ExtendedPoint.ZERO)
+    ).to.be.true;
+
+    const circomHelper = new CircomStuff(
+      await ethers.provider.getSigner(),
+      rkAddress
+    );
+
+    circomHelper.verifySig(
+      toStr(s),
+      [toStr(R.x), toStr(R.y)],
+      [toStr(A.x), toStr(A.y)],
+      Buffer.from(msgBytes).toString("hex")
+    );
+  });
+
+  it("transact", async () => {
+    const [privateKey, recieverPrivateKey, b1] = getRandomBits(10, 253);
+    const spendKey = poseidonHash([privateKey]);
+    const receiverSpendKey = poseidonHash([recieverPrivateKey]);
+
+    const spendList: Note[] = [
+      {
+        amount: 10n,
+        asset: await getAsset("USDC"),
+        spender: spendKey,
+        blinding: toFixedHex(b1),
+      },
+    ];
+
+    const nc = await notecommitment(spendList[0]);
+    const tree = new MerkleTree(5, [], {
+      hashFunction: poseidonHash2,
     });
+
+    tree.insert(nc);
+
+    await transfer(
+      await ethers.provider.getSigner(),
+      rkAddress,
+      10n,
+      privateKey,
+      spendKey,
+      receiverSpendKey,
+      "USDC",
+      tree,
+      {
+        async getNotesUpTo(_amount: bigint, _asset: string) {
+          return spendList;
+        },
+      }
+    );
+  });
+
+  it("deposit", async () => {
+    const [privateKey, recieverPrivateKey, b1] = getRandomBits(10, 253);
+    const spendKey = poseidonHash([privateKey]);
+    const receiverSpendKey = poseidonHash([recieverPrivateKey]);
+
+    const spendList: Note[] = [
+      {
+        amount: 10n,
+        asset: await getAsset("USDC"),
+        spender: spendKey,
+        blinding: toFixedHex(b1),
+      },
+    ];
+
+    const nc = await notecommitment(spendList[0]);
+
+    const tree = new MerkleTree(5, [], {
+      hashFunction: poseidonHash2,
+    });
+
+    tree.insert(nc);
+
+    await deposit(
+      await ethers.provider.getSigner(),
+      rkAddress,
+      10n,
+      privateKey,
+      receiverSpendKey,
+      "USDC",
+      tree
+    );
+  });
+
+  it("withdrawal", async () => {
+    const [privateKey, recieverPrivateKey, b1] = getRandomBits(10, 253);
+    const spendKey = poseidonHash([privateKey]);
+    const receiverSpendKey = poseidonHash([recieverPrivateKey]);
+
+    const spendList: Note[] = [
+      {
+        amount: 10n,
+        asset: await getAsset("USDC"),
+        spender: spendKey,
+        blinding: toFixedHex(b1),
+      },
+    ];
+
+    const nc = await notecommitment(spendList[0]);
+    const tree = new MerkleTree(5, [], {
+      hashFunction: poseidonHash2,
+    });
+
+    tree.insert(nc);
+
+    await withdraw(
+      await ethers.provider.getSigner(),
+      rkAddress,
+      10n,
+      privateKey,
+      spendKey,
+      receiverSpendKey,
+      "USDC",
+      tree,
+      {
+        async getNotesUpTo(_amount: bigint, _asset: string) {
+          return spendList;
+        },
+      }
+    );
+  });
+
+  const outputProve = async (
+    amount: string,
+    blinding: string,
+    asset: string,
+    publicKey: string,
+    Vx: string,
+    Vy: string,
+    Rx: string,
+    Ry: string,
+    r: string,
+    Cx: string,
+    Cy: string
+  ) => {
+    return await generateGroth16Proof(
+      {
+        amount,
+        blinding,
+        asset,
+        publicKey,
+        Vx,
+        Vy,
+        Rx,
+        Ry,
+        r,
+        Cx,
+        Cy,
+      },
+      "output"
+    );
   };
 
-  const createStore = (hexPrivate: string) => {
-    let store: WalletStore = {
-      notes: [],
-      nullifiers: [],
-      privateKey: hexPrivate,
-      async getNotesUpTo(amount: bigint, asset: string) {
-        const assetId = await getAsset(asset);
-        const notesOfAsset = getUnspentNotes(store).filter(
-          (n) => n.note.asset === assetId
-        );
-        let total = 0n;
-        let notes: Note[] = [];
-        for (let { note } of notesOfAsset) {
-          if (note.amount === 0n) continue;
-          total += note.amount;
-          notes.push(note);
-          if (total > amount) break;
-        }
-        return notes;
-      },
-      async getBalance(asset: string) {
-        const assetId = await getAsset(asset);
-        const notesOfAsset = getUnspentNotes(store).filter(
-          (n) => n.note.asset === assetId
-        );
-        return notesOfAsset.reduce((total, note) => {
-          return total + note.note.amount;
-        }, 0n);
-      },
-      async logBalances() {
-        const assets = ["USDC", "WBTC"];
-        const balances = await Promise.all(
-          assets.map((asset) => this.getBalance(asset))
-        );
-        console.log("Balance");
-        console.table(
-          balances.map((bal, i) => ({
-            Asset: assets[i],
-            Balance: bal,
-          }))
-        );
-      },
+  it("output", async () => {
+    const { R, valcommit, getV } = getInitialPoints(babyJub);
+    const [privateKey, b1] = getRandomBits(10, 253);
+    const spendKey = poseidonHash([privateKey]);
+
+    const n1: Note = {
+      amount: 10n,
+      asset: await getAsset("USDC"),
+      spender: spendKey,
+      blinding: toFixedHex(b1),
     };
-    return store;
-  };
 
-  const getNoteCommitmentEvents = (
-    receipt: ContractTransactionReceipt | null
-  ) => {
-    if (!receipt) throw new Error("receipt was null!");
+    const n1nc = await notecommitment(n1);
+    const { Vc: n1vc, r: r1 } = valcommit(n1);
 
-    const decodedLogs = receipt?.logs
-      .map((log) => {
-        try {
-          const sm = new Interface(RK__factory.abi);
-          return sm.parseLog(log);
-        } catch (error) {
-          // This log was not from your contract, or not an event your contract emits
-          return null;
-        }
-      })
-      .filter((log) => log !== null);
-    return decodedLogs.filter((c) => c !== null) as LogDescription[];
-  };
-
-  const extractToStore = async (
-    hexPrivate: string,
-    store: WalletStore,
-    receipt: ContractTransactionReceipt | null
-  ) => {
-    const events = getNoteCommitmentEvents(receipt);
-    for (let ev of events) {
-      if (ev.name === "NewCommitment") {
-        const cypher = ev.args[2] as string;
-        const index = ev.args[1] as bigint;
-        try {
-          const note = await decryptNote(hexPrivate, cypher);
-          const nullifier = await nullifierHash(
-            "0x" + store.privateKey,
-            note,
-            index
-          );
-          store.notes.push({
-            index: ev.args[1],
-            nullifier,
-            note,
-          });
-        } catch (err) {}
-      }
-      if (ev.name === "NewNullifier") {
-        store.nullifiers.push(ev.args[0].toString());
-      }
-    }
-    return store;
-  };
-
-  it.only("integrate", async () => {
-    // for this test the user needs USDC and WBTC
-    const usdcAmount = parseUnits("100", 6);
-    await testUSDC.mint(Deployer.address, usdcAmount);
-    const wbtcAmount = parseUnits("100", 18);
-    await testWBTC.mint(Deployer.address, wbtcAmount);
-
-    // we also need to approve the RK address to move these amounts
-    await testUSDC.approve(rkAddress, usdcAmount);
-    await testWBTC.approve(rkAddress, wbtcAmount);
-
-    let tree = await buildMerkleTree(RK as any as Contract);
-
-    const spenderPrivate = Wallet.createRandom().privateKey;
-    const receiverPrivate = Wallet.createRandom().privateKey;
-    const receiver = await getKeys(BigInt(receiverPrivate));
-    const spender = await getKeys(BigInt(spenderPrivate));
-    const hexPrivate = spender.privateKey.toString(16);
-
-    // store is how we track our users encrypted notes/balances
-    let store = createStore(hexPrivate);
-    await store.logBalances();
-
-    // first as the user, we need to deposit our USDC
-    let tx = await deposit(
-      await ethers.provider.getSigner(),
-      rkAddress,
-      usdcAmount,
-      spender,
-      "USDC",
-      tree
-    );
-    let receipt = await tx.wait();
-
-    // our users USDC balance should have gone down
-    expect(await testUSDC.balanceOf(Deployer.address)).to.equal(0);
-    // and our contracts balance should have gone up
-    expect(await testUSDC.balanceOf(rkAddress)).to.equal(usdcAmount);
-
-    store = await extractToStore(hexPrivate, store, receipt);
-    await store.logBalances();
-
-    // next, we deposit our WBTC
-    tx = await deposit(
-      await ethers.provider.getSigner(),
-      rkAddress,
-      wbtcAmount,
-      spender,
-      "WBTC",
-      tree
+    const V = getV(n1.asset);
+    const proof = await outputProve(
+      toStr(n1.amount),
+      n1.blinding,
+      n1.asset,
+      n1.spender,
+      toStr(V.x),
+      toStr(V.y),
+      toStr(R.x),
+      toStr(R.y),
+      toStr(r1),
+      toStr(n1vc.x),
+      toStr(n1vc.y)
     );
 
-    // our users WBTC balance should have gone down
-    expect(await testWBTC.balanceOf(Deployer.address)).to.equal(0);
-    // and our contracts balance should have gone up
-    expect(await testWBTC.balanceOf(rkAddress)).to.equal(wbtcAmount);
-
-    receipt = await tx.wait();
-    store = await extractToStore(hexPrivate, store, receipt);
-    await store.logBalances();
-
-    tree = await buildMerkleTree(RK as any as Contract);
-
-    const transferAmountUSDC = parseUnits("10", 6);
-
-    // now that we have our assets deposited/shielded, we can transfer them through the protocol
-    tx = await transfer(
-      await ethers.provider.getSigner(),
-      rkAddress,
-      transferAmountUSDC,
-      spender,
-      receiver,
-      "USDC",
-      tree,
-      store
-    );
-
-    receipt = await tx.wait();
-    store = await extractToStore(hexPrivate, store, receipt);
-    await store.logBalances();
-    tree = await buildMerkleTree(RK as any as Contract);
-
-    const usdcWithdrawalAmount = parseUnits("50", 6);
-
-    // and, we can withdraw them too
-    tx = await withdraw(
-      await ethers.provider.getSigner(),
-      rkAddress,
-      usdcWithdrawalAmount,
-      spender,
-      receiver,
-      "USDC",
-      tree,
-      store
-    );
-
-    // the contracts USDC balance should have gone down
-    expect(await testUSDC.balanceOf(rkAddress)).to.equal(
-      usdcAmount - usdcWithdrawalAmount
-    );
-
-    // the users USDC balance should have gone up
-    expect(await testUSDC.balanceOf(Deployer.address)).to.equal(
-      usdcWithdrawalAmount
-    );
-
-    receipt = await tx.wait();
-    store = await extractToStore(hexPrivate, store, receipt);
-    await store.logBalances();
+    await RK.outputVerify(proof, [n1nc]);
   });
 });
