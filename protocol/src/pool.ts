45559c1ce8b88fb6e6815c9343b0d89d6d7d2e93
ryardley <contact@rudiyardley.com>
2024-05-10 18:07:35 +1000

    Remove contract class

--- protocol/src/pool.ts
+++ protocol/src/pool.ts
@@ -2,7 +2,7 @@ import { Signer } from "ethers";
 import { IMasp__factory, MultiAssetShieldedPool__factory } from "../typechain-types";
 import { OutputProof, SpendProof } from ".";
 
-
+// These are more for testing
 export async function outputVerify(
   address: string,
   provider: Signer,
@@ -13,6 +13,7 @@ export async function outputVerify(
   return await verifier.outputVerify(proof, [commitment]);
 }
 
+// These are more for testing
 export async function spendVerify(
   address: string,
   provider: Signer,
@@ -22,82 +23,3 @@ export async function spendVerify(
   const verifier = MultiAssetShieldedPool__factory.connect(address, provider);
   return await verifier.spendVerify(proof, [commitment]);
 }
-
-export class MaspContract {
-  constructor(private provider: Signer, private address: string) {}
-
-  async deposit(
-    spends: SpendProof[],
-    outputs: OutputProof[],
-    Bpk: [string, string],
-    assetId: string,
-    amount: string,
-    root: string,
-    sigRx: string,
-    sigRy: string,
-    sigS: string,
-    hash: string
-  ) {
-    const verifier = IMasp__factory.connect(this.address, this.provider);
-    return await verifier.deposit(
-      spends,
-      outputs,
-      Bpk,
-      assetId,
-      amount,
-      root,
-      [sigRx, sigRy],
-      sigS,
-      hash
-    );
-  }
-
-  async withdraw(
-    spends: SpendProof[],
-    outputs: OutputProof[],
-    Bpk: [string, string],
-    assetId: string,
-    amount: string,
-    root: string,
-    sigRx: string,
-    sigRy: string,
-    sigS: string,
-    hash: string
-  ) {
-    const verifier = IMasp__factory.connect(this.address, this.provider);
-    return await verifier.withdraw(
-      spends,
-      outputs,
-      Bpk,
-      assetId,
-      amount,
-      root,
-      [sigRx, sigRy],
-      sigS,
-      hash
-    );
-  }
-
-  async transact(
-    spends: SpendProof[],
-    outputs: OutputProof[],
-    Bpk: [string, string],
-    root: string,
-    sigRx: string,
-    sigRy: string,
-    sigS: string,
-    hash: string
-  ) {
-    const verifier = IMasp__factory.connect(this.address, this.provider);
-    return await verifier.transact(
-      spends,
-      outputs,
-      Bpk,
-      root,
-      [sigRx, sigRy],
-      sigS,
-      hash
-    );
-  }
-}
-
